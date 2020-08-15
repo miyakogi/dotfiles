90f1be64dacef7d41b707c29693b8ebd9ea29b7f
miyakogi <miyako.dev@gmail.com>
2020-08-15 17:47:32 +0900

    [Picom] Draw shadow with i3-gaps
    - launch picom in the polybar's launch script
    - control picom shadow by launch option
    - increase top gap on i3

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,15 +2,19 @@
 
 # Terminate already running bar instancees
 killall -q polybar
+killall -q picom
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 0.1; done
+while pgrep -u $UID -x picom >/dev/null; do sleep 0.1; done
 
 # Launch Polybar
 if grep "gaps" ~/.config/i3/config >/dev/null; then
   polybar left &
   polybar center &
   polybar right &
+  picom -cb &
 else
   polybar top &
+  picom -b &
 fi
