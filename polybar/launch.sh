644ee12225760aa8d31b9bcfa2549c06abd9a76c
miyakogi <miyako.dev@gmail.com>
2020-08-23 17:00:19 +0900

    [Polybar/i3/bspwm] support gaps/no-gaps swapping on bspwm

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -9,12 +9,13 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 0.1; done
 while pgrep -u $UID -x picom >/dev/null; do sleep 0.1; done
 
 # Launch Polybar
+if wmctrl -m | grep "bspwm" >/dev/null; then wm="bspwm"; else wm="i3"; fi
 if grep "gaps" ~/.config/i3/config >/dev/null; then
-  polybar left &
-  polybar center &
-  polybar right &
+  polybar "left-$wm" &
+  polybar "center-$wm" &
+  polybar "right-$wm" &
   picom -cb &
 else
-  polybar top &
+  polybar "top-$wm" &
   picom -b &
 fi
