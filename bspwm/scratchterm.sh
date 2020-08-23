473b2a357c0db1f6b6a3d6a58adb9430e6488d59
miyakogi <miyako.dev@gmail.com>
2020-08-24 06:49:08 +0900

    [bspwm] should not use $TERM in shell script

--- bspwm/scratchterm.sh
+++ bspwm/scratchterm.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-TERM=konsole
-TERM_TITLE=scratchkonsole
+term=konsole
+term_title=scratchkonsole
 
-wid=$(xdotool search --name $TERM_TITLE)
+wid=$(xdotool search --name $term_title)
 if [[ -z $wid ]]; then
-  env DROPDOWN=1 $TERM --title $TERM_TITLE &
-  wid=`xdotool search --sync --name $TERM_TITLE`
+  env DROPDOWN=1 $term --title $term_title &
+  wid=`xdotool search --sync --name $term_title`
   bspc node $wid --flag hidden -f
 else
   if [[ `xdotool getactivewindow` -eq $wid ]]; then
     bspc node $wid --flag hidden -f
   else
-    if [[ ! `xdotool search --onlyvisible --name $TERM_TITLE` ]]; then
+    if [[ ! `xdotool search --onlyvisible --name $term_title` ]]; then
       bspc node $wid --flag hidden -f
     fi
-    xdotool search --name $TERM_TITLE windowactivate
+    xdotool search --name $term_title windowactivate
   fi
 fi
