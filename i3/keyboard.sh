ace048436b25a442967838dae68202350a07b96d
miyakogi <miyako.dev@gmail.com>
2020-08-25 14:18:39 +0900

    [bspwm] start sxhkd in keyboard script

--- i3/keyboard.sh
+++ i3/keyboard.sh
@@ -5,3 +5,12 @@ sleep 1
 if test -f $HOME/.Xkeymap; then
   xkbcomp $HOME/.Xkeymap $DISPLAY
 fi
+
+# start sxhkd for bspwm
+if wmctrl -m | grep bspwm > /dev/null; then
+  if pgrep -x sxhkd > /dev/null; then
+    pkill -USR1 -x sxhkd
+  else
+    sxhkd -c ~/.config/bspwm/sxhkdrc
+  fi
+fi
