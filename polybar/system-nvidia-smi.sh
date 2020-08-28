1c96d9c8f84498c268f6de53309c6a2f40f6e993
miyakogi <miyako.dev@gmail.com>
2020-08-29 06:19:45 +0900

    [Polybar] remove space before GPU percentage

--- polybar/system-nvidia-smi.sh
+++ polybar/system-nvidia-smi.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # from https://github.com/polybar/polybar-scripts/blob/master/polybar-scripts/system-nvidia-smi/system-nvidia-smi.sh
 
-nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk '{ print ""$1"","%"}'
+echo $(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits)%
