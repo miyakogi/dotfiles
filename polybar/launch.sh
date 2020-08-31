8c0f56de9f7ea757b984a868bf684f9ac83aff02
miyakogi <miyako.dev@gmail.com>
2020-08-31 15:10:55 +0900

    [Polybar] support kwin

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -12,6 +12,8 @@ while pgrep -u $UID -x picom >/dev/null; do sleep 0.1; done
 if wmctrl -m | grep "bspwm" >/dev/null; then
   wm="bspwm"
   picom_opt="-f"
+elif wmctrl -m | grep "KWin" >/dev/null; then
+  wm="kwin"
 else
   wm="i3"
   picom_opt=""
@@ -21,8 +23,12 @@ if grep "gaps" ~/.config/i3/config >/dev/null; then
   polybar "left-$wm" &
   polybar "center-$wm" &
   polybar "right-$wm" &
-  picom -cb $picom_opt --experimental-backends &
+  if [[ $wm != "kwin" ]]; then
+    picom -cb $picom_opt --experimental-backends &
+  fi
 else
   polybar "top-$wm" &
-  picom -b $picom_opt --experimental-backends &
+  if [[ $wm != "kwin" ]]; then
+    picom -b $picom_opt --experimental-backends &
+  fi
 fi
