7d7896648e0777569daed5bcbb996a03d14cf934
miyakogi <miyako.dev@gmail.com>
2020-08-31 16:20:50 +0900

    [Rofi] support kwin on exit menu

--- rofi/leave.sh
+++ rofi/leave.sh
@@ -7,25 +7,51 @@ WM=$(wmctrl -m | grep "Name" | sed -r 's/^Name: ([a-zA-Z0-9\-_]+)$/\1/')
 menu="Lock\nExit\nRestart\nShutdown"
 RET=$(echo -e $menu | rofi -dmenu -i -p "Select" -theme leave-theme)
 
+function lock() {
+  ~/.config/i3/lock.sh
+}
+
+function _exit() {
+  killall redshift
+  case $WM in
+    awesome)
+      awesome-client "awesome.quit()";;
+    bspwm)
+      bspc quit;;
+    i3)
+      i3-msg exit;;
+    KWin)
+      lxqt-leave --logout;;
+    *)
+      ;;
+  esac
+}
+
+function reboot() {
+  if [[ $WM = "KWin" ]]; then
+    lxqt-leave --reboot
+  else
+    systemctl reboot
+  fi
+}
+
+function shutdown() {
+  if [[ $WM = "KWin" ]]; then
+    lxqt-leave --shutdown
+  else
+    systemctl poweroff -i
+  fi
+}
+
 case $RET in
   Lock)
-    ~/.config/i3/lock.sh;;
+    lock;;
   Exit)
-    killall redshift;
-    case $WM in
-      awesome)
-        awesome-client "awesome.quit()";;
-      bspwm)
-        bspc quit;;
-      i3)
-        i3-msg exit;;
-      *)
-        ;;
-    esac ;;
+    _exit;;
   Restart)
-    systemctl reboot;;
+    reboot;;
   Shutdown)
-    systemctl poweroff -i;;
+    shutdown;;
   *)
     ;;
 esac
