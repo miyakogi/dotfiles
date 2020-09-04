c498e6c0c60d8846e309246b2c4cbabb35386bbf
miyakogi <miyako.dev@gmail.com>
2020-09-05 06:13:09 +0900

    [Krohnkite] improve control script

--- scripts/krohnkite-control.sh
+++ scripts/krohnkite-control.sh
@@ -1,20 +1,39 @@
 #!/bin/sh
 
-# check executables
-if ! which kwriteconfig5 > /dev/null || ! which qdbus > /dev/null || wmctrl -m | grep "Name:" | grep -v "KWin" > /dev/null; then
-  exit
+# Check executables
+function check_executable() {
+  if ! which $1 >/dev/null 2>&1; then
+    echo "Need $1 command"
+    exit 1
+  fi
+}
+
+check_executable "wmctrl"
+check_executable "qdbus"
+check_executable "kwriteconfig5"
+check_executable "kreadconfig5"
+
+# Check window manager is kwin
+if wmctrl -m | grep "Name:" | grep -v -i "kwin" > /dev/null; then
+  echo "Current window manager is not Kwin"
+  exit 1
 fi
 
-if [[ $1 = "enable" ]]; then
-  types='305'
-  enabled='true'
-elif [[ $1 = "disable" ]]; then
-  types='4'
-  enabled='false'
-else
-  echo "Valid commands are only [enable|disable]"
-  exit
-fi
+# Set variables
+case $1 in
+  enable)
+    types='305'
+    enabled='true'
+    ;;
+  disable)
+    types='4'
+    enabled='false'
+    ;;
+  *)
+    echo "Valid commands are [enable|disable]"
+    exit
+    ;;
+esac
 
 # Disable/Enable window border and title bar
 rulesrc="$HOME/.config/kwinrulesrc"
@@ -32,7 +51,7 @@ fi
 # Enable/Disable krohnkite
 kwinrc="$HOME/.config/kwinrc"
 if grep "krohnkiteEnabled" $kwinrc > /dev/null; then
-  kwriteconfig5 --file $kwinrc --group Plugins --key 'krohnkiteEnabled' $enabled
+  kwriteconfig5 --file $kwinrc --group Plugins --type bool --key 'krohnkiteEnabled' $enabled
 fi
 
 # Reconfigure kwin
