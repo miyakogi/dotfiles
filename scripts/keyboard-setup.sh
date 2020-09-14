6d94584fc392ff88d4ddd88ffa14c5077c9daa05
miyakogi <miyako.dev@gmail.com>
2020-09-14 18:21:10 +0900

    [Script/bspwm] move sxhkd to bspwmrc

--- scripts/keyboard-setup.sh
+++ scripts/keyboard-setup.sh
@@ -1,19 +1,4 @@
 #!/usr/bin/env zsh
 
-WM=$(wmctrl -m | grep "Name: " | sed 's/^Name: \(.\+\)$/\1/' | tr '[:upper:]' '[:lower:]')
-
 setxkbmap -layout jp -option 'ctrl:nocaps'
-
-if [[ -f $HOME/.Xkeymap ]]; then
-  sleep 1
-  xkbcomp $HOME/.Xkeymap $DISPLAY
-fi
-
-# start sxhkd for bspwm
-if [[ $WM == bspwm ]]; then
-  if pgrep -x sxhkd > /dev/null; then
-    pkill -USR1 -x sxhkd
-  else
-    sxhkd -c ~/.config/bspwm/sxhkdrc
-  fi
-fi
+test -f $HOME/.Xkeymap && sleep 1 && xkbcomp $HOME/.Xkeymap $DISPLAY
