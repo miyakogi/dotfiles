d99d6e01a7f59786adfb7fbac1916349b205f9a8
miyakogi <miyako.dev@gmail.com>
2020-10-04 08:36:28 +0900

    [Picom] remove fade option from bspwm

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -10,20 +10,17 @@ while pgrep -u $UID -x picom >/dev/null; do sleep 0.1; done
 
 # get window manager name
 wm=$(wmctrl -m | grep "Name:" | sed 's/^Name: \(.\+\)$/\1/' | tr '[:upper:]' '[:lower:]')
-if [[ $wm == "bspwm" ]]; then
-  picom_opt="-f"
-fi
 
 if grep -q "gaps" ${XDG_CONFIG_HOME:-$HOME/.config}/i3/config; then
   polybar "left-$wm" &
   polybar "center-$wm" &
   polybar "right-$wm" &
   if [[ $wm != "kwin" ]]; then
-    picom -b --corner-radius 12.0 $picom_opt --experimental-backends $QEMU_PICOM_OPTION &
+    picom -b --corner-radius 12.0 --experimental-backends $QEMU_PICOM_OPTION &
   fi
 else
   polybar "top-$wm" &
   if [[ $wm != "kwin" ]]; then
-    picom -b $picom_opt --experimental-backends $QEMU_PICOM_OPTION &
+    picom -b --experimental-backends $QEMU_PICOM_OPTION &
   fi
 fi
