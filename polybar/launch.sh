247b530c2b7a0bc3fd0e168c416151c387eb0ad9
miyakogi <miyako.dev@gmail.com>
2020-12-25 16:51:27 +0900

    [Picom/Polybar] not use rounded corners
    - Use a bit more transparent color for gapped polybar
    - Remove borders on gapped polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,11 +2,9 @@
 
 # Terminate already running bar instancees
 killall -q polybar
-killall -q picom
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 0.1; done
-while pgrep -u $UID -x picom >/dev/null; do sleep 0.1; done
 
 # get window manager name
 wm=$(wmctrl -m | grep "Name:" | sed 's/^Name: \(.\+\)$/\1/' | tr '[:upper:]' '[:lower:]')
@@ -15,12 +13,6 @@ if grep -q "gaps" ${XDG_CONFIG_HOME:-$HOME/.config}/i3/config; then
   polybar "left-$wm" &
   polybar "center-$wm" &
   polybar "right-$wm" &
-  if [[ $wm != "kwin" ]]; then
-    picom -b --corner-radius 12.0 --experimental-backends &
-  fi
 else
   polybar "top-$wm" &
-  if [[ $wm != "kwin" ]]; then
-    picom -b --experimental-backends &
-  fi
 fi
--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -48,6 +48,9 @@ else
   # need to set $LATITUDE and $LONGITUDE in ~/.zsh/.zshenv.local
   # run redshift -l ${LATITUDE}:${LONGITUDE} -t 6500:5200
   run redshift -l ${LATITUDE}:${LONGITUDE}
+
+  # picom compositor
+  run picom -b --experimental-backend
 fi
 
 if [[ -z $DE ]]; then
