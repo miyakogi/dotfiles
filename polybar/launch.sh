0bd8a7640111b4baf66cb6631d4db9c5b5eaff27
miyakogi <miyako.dev@gmail.com>
2020-12-25 11:31:53 +0900

    [Picom] remove QEMU option support

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -16,11 +16,11 @@ if grep -q "gaps" ${XDG_CONFIG_HOME:-$HOME/.config}/i3/config; then
   polybar "center-$wm" &
   polybar "right-$wm" &
   if [[ $wm != "kwin" ]]; then
-    picom -b --corner-radius 12.0 --experimental-backends $QEMU_PICOM_OPTION &
+    picom -b --corner-radius 12.0 --experimental-backends &
   fi
 else
   polybar "top-$wm" &
   if [[ $wm != "kwin" ]]; then
-    picom -b --experimental-backends $QEMU_PICOM_OPTION &
+    picom -b --experimental-backends &
   fi
 fi
