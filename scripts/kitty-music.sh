55bb4479c6783eaaa26b0250002a25146634693f
miyakogi <miyako.dev@gmail.com>
2021-01-14 21:57:50 +0900

    [Kitty] change music player layout and disabled remote control by default

--- scripts/kitty-music.sh
+++ scripts/kitty-music.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env zsh
 # Should be executed by kitty with remote control enabled
-# Ex: `kitty --title kitty-music /path/to/kitty-music.sh`
+# Ex: `kitty -T kitty-music -o allow_remote_control=yes -o enabled_layouts="*" kitty-music.sh`
 
-kitty @ launch --title ncmpcpp ncmpcpp
-kitty @ launch --title mpdcover mpdcover
-kitty @ launch --title cava cava
+kitty @ goto-layout splits
+kitty @ launch --title ncmpcpp --location vsplit --keep-focus ncmpcpp
+kitty @ launch --title cava --location hsplit --keep-focus cava
+kitty @ launch --title mpdcover --location hsplit --keep-focus mpdcover
 kitty @ resize-window --match title:ncmpcpp --increment 45
