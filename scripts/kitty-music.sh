646f51e7bc020e5ed1ea96706305aaf8ebb56df4
miyakogi <miyako.dev@gmail.com>
2021-01-14 19:29:40 +0900

    [Kitty] use remote control to launch music player/viewers

--- scripts/kitty-music.sh
+++ scripts/kitty-music.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env zsh
-kitty --title kitty-music --session - <<EOF
-launch zsh -c ncmpcpp
-launch mpdcover
-launch cava
-EOF
+# Should be executed by kitty with remote control enabled
+# Ex: `kitty --title kitty-music /path/to/kitty-music.sh`
+
+kitty @ launch --title ncmpcpp ncmpcpp
+kitty @ launch --title mpdcover mpdcover
+kitty @ launch --title cava cava
+kitty @ resize-window --match title:ncmpcpp --increment 45
