e25cc53c203da33feda67f81fe3b478fa86a9ab4
miyakogi <miyako.dev@gmail.com>
2021-03-22 17:30:48 +0900

    [Fish] update C-y keybind

--- fish/conf.d/keybind.fish
+++ fish/conf.d/keybind.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
 function fish_user_key_bindings
-  bind \cy 'cd ../ ; echo "" ; commandline -f repaint'
+  bind \cy 'cd ../ ; echo "" ; echo "" ; commandline -f repaint'
   bind \cj myjump
   bind \cr search-history
   bind \cf nextd-or-forward-word
