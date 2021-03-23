bf780a264585de4921674792d4f4373ca93983e2
miyakogi <miyako.dev@gmail.com>
2021-03-23 12:39:57 +0900

    [fish] use abbr instead of alias

--- fish/conf.d/alias.fish
+++ fish/conf.d/alias.fish
@@ -1,42 +1,42 @@
 #!/usr/bin/env fish
 
 # vim-style exit
-alias :q "exit"
+abbr --add --global :q "exit"
 
 # mkdir -p
-alias mkdir "mkdir -p"
+abbr --add --global mkdir "mkdir -p"
 
 # tmux
-alias tmux "env TERM=xterm-256color tmux -2"
+abbr --add --global tmux "env TERM=xterm-256color tmux -2"
 
 # ln -v
-alias ln "ln -v"
+abbr --add --global ln "ln -v"
 
 # vi
-alias vi "vim --no-plugin"
+abbr --add --global vi "vim --no-plugin"
 
 # git
-alias gstatus "git status -s -b"
+abbr --add --global gstatus "git status -s -b"
 
 # vim/nvim
 if __which nvim; and test $XDG_SESSION_TYPE = wayland
-  alias vim nvim
+  abbr --add --global vim nvim
   set -x EDITOR nvim
 end
 
 # ls -> exa/lsd
 if __which exa
-  alias ls "exa --icons"
+  abbr --add --global ls "exa --icons"
 else if __which lsd
-  alias ls lsd
+  abbr --add --global ls lsd
 end
 
 # cat -> bat
 if __which bat
-  alias cat "env PAGER=less bat"
+  abbr --add --global cat "env PAGER=less bat"
 end
 
 # ssh
 if test $TERM = "alacritty"; or test $TERM = "kitty"; or test $TERM = "foot"
-  alias ssh "env TERM=xterm-256color ssh"
+  abbr --add --global ssh "env TERM=xterm-256color ssh"
 end
