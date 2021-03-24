0a97b83e7f1258fa8c8a9f472bac567a416914dc
miyakogi <miyako.dev@gmail.com>
2021-03-24 14:14:43 +0900

    [fish] simplify chpwd function

--- fish/functions/chpwd.fish
+++ fish/functions/chpwd.fish
@@ -1,9 +1,9 @@
 function chpwd
-  set -l curdir (echo "$PWD" | \
+  set -l curdir (pwd | \
     string replace "$HOME" "\$HOME" | \
     string replace "$USER" "\$USER")
   begin
-    cat "$HIST_DIRS_FILE" | grep -v -x "$curdir"
+    grep -v -x "$curdir" "$HIST_DIRS_FILE"
     echo "$curdir"
   end | string collect | tail -n $HIST_DIRS_MAX >"$HIST_DIRS_FILE"
 end
