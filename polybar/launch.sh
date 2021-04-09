4e20bfaa4bc4f02d9e68517b695bcb4e72f29c41
miyakogi <miyako.dev@gmail.com>
2021-04-09 10:40:33 +0900

    [Polybar] remove gapped variants

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -9,10 +9,5 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 0.1; done
 # get window manager name
 wm=$(wmctrl -m | grep "Name:" | sed 's/^Name: \(.\+\)$/\1/' | tr '[:upper:]' '[:lower:]')
 
-if grep -q "gaps" ${XDG_CONFIG_HOME:-$HOME/.config}/i3/config; then
-  polybar "left-$wm" &
-  polybar "center-$wm" &
-  polybar "right-$wm" &
-else
-  polybar "top-$wm" &
-fi
+# launch proper bar
+polybar "top-$wm" &
