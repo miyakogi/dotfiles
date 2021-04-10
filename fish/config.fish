6d80a577127189e78368414f586794fe6cbc2cfa
miyakogi <miyako.dev@gmail.com>
2021-04-11 07:10:55 +0900

    [fish] set ls colors

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,9 @@
 #!/usr/bin/env fish
 
+# ls color setting
+set -x LSCOLORS Exfxcxdxbxegedabagacad
+set -x LS_COLORS 'di=01;34:ln=01;35:so=01;32:ex=01;31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
+
 # set skim/percol search command
 if type -q sk
   set _search_cmd "sk --tac --no-sort --reverse --exact --tiebreak index --ansi -p 'skim>' --margin 2% --query (commandline -b)"
