d974f79d471dcacc4756bd74fe94efd2c4fb7916
miyakogi <miyako.dev@gmail.com>
2021-05-19 06:38:41 +0900

    [Waybar] simplify to reload waybar

--- waybar/launch.sh
+++ waybar/launch.sh
@@ -1,10 +1,7 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
-# Terminate current running waybar instances
-killall waybar
-
-# Wait until the processes to be shut down
-while pgrep -x waybar >/dev/null; do sleep 0.1; done
-
-# Launch waybar
-waybar &
+if pgrep -x waybar >/dev/null; then
+  killall -SIGUSR2 waybar
+else
+  waybar &
+fi
