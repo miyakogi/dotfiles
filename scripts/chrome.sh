45a1acb682257231dce0741d1c33c42b46b2c3ca
miyakogi <miyako.dev@gmail.com>
2021-05-21 03:52:51 +0900

    [Script] enable HW video acceleration on chrome.sh script
    - Enable vulkan on both X11 and wayland
    - Disable ozone on wayland by default
    - To enable ozone, need to add wayland flag as the first argument

--- scripts/chrome.sh
+++ scripts/chrome.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 # This script enables chrome's HW acceleration on both Wayland and Xorg sessions
 
 if [[ $XDG_SESSION_TYPE == "wayland" ]]; then
@@ -7,14 +7,22 @@ else
   session=$(loginctl show-session $(loginctl show-user $(whoami) -p Display --value) -p Type --value)
 fi
 
-flags=("--ignore-gpu-blocklist" "--enable-gpu-rasterization" "--enable-zero-copy")
+flags=("--ignore-gpu-blocklist" "--enable-gpu-rasterization" "--enable-zero-copy" "--enable-accelerated-video-decoder")
+features="VaapiVideoDecoder,Vulkan"
+options=("$@")
+
+if [[ $1 == "wayland" ]]; then
+  features="$features,UseOzonePlatform,WebRTCPipeWireCapturer"
+  flags+=("--ozone-platform=wayland")
+  options=("${options[2,-1]}")  # remove $1
+fi
 
 if [[ $session == "wayland" ]]; then
   # Wayland session
-  flags+=("--enable-features=UseOzonePlatform,WebRTCPipeWireCapturer" "--ozone-platform=wayland")
+  flags+=("--enable-features=$features")
 else
   # Xorg session
-  flags+=("--use-gl=desktop")
+  flags+=("--enable-features=$features" "--use-gl=desktop")
 fi
 
-exec google-chrome-stable $flags $@
+exec google-chrome-stable "${flags[@]}" "${options[@]}"
