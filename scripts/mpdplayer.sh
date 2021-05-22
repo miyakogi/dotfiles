881b5e2579d7de5825227be2b45ce622b22f78a3
miyakogi <miyako.dev@gmail.com>
2021-05-23 08:42:27 +0900

    [Script] fix mpd player font

--- scripts/mpdplayer.sh
+++ scripts/mpdplayer.sh
@@ -5,6 +5,6 @@ options=(
   --override background_opacity=0.7
   --override allow_remote_control=yes
   --override enabled_layouts='*'
-  --override font_family='Fira Code Regular'
+  --override font_family='Fira Code'
 )
 kitty "${options[@]}" kitty-music
