e2d0bb840cf5cef40133a61a422bdde43eb85bd4
miyakogi <miyako.dev@gmail.com>
2021-09-25 23:10:15 +0900

    [scripts] refactor chrome startup script

--- scripts/chrome.sh
+++ scripts/chrome.sh
@@ -7,21 +7,32 @@ else
   session=$(loginctl show-session $(loginctl show-user $(whoami) -p Display --value) -p Type --value)
 fi
 
-flags=("--ignore-gpu-blocklist" "--enable-gpu-rasterization" "--enable-zero-copy" "--enable-accelerated-video-decoder")
+flags=(
+  --ignore-gpu-blocklist
+  --enable-gpu-rasterization
+  --enable-zero-copy
+  --enable-accelerated-video-decoder
+)
 features="VaapiVideoDecoder,Vulkan"
 options=("$@")
 
 if [[ $1 == "wayland" ]]; then
   # Native wayland
   features="$features,UseOzonePlatform,WebRTCPipeWireCapturer"
-  flags+=("--enable-features=$features" "--ozone-platform=wayland")
-  options=("${options[2,-1]}")  # remove $1
-elif [[ $session == "wayland" ]]; then
-  # XWayland
-  flags+=("--enable-features=$features" "--use-gl=egl")
+  flags+=(
+    --enable-features="$features"
+    --ozone-platform=wayland
+  )
+  options=("${options[2,-1]}")  # remove first option ($1)
 else
-  # Xorg session
-  flags+=("--enable-features=$features" "--use-gl=desktop")
+  flags+=(--enable-features="$features")
+  if [[ $session == "wayland" ]]; then
+    # XWayland
+    flags+=(--use-gl=egl)
+  else
+    # Xorg session
+    flags+=(--use-gl=desktop)
+  fi
 fi
 
 exec google-chrome-stable "${flags[@]}" "${options[@]}"
