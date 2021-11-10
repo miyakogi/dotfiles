1c8ca0806f1428464457b7ab470cc2598190b777
miyakogi <miyako.dev@gmail.com>
2021-11-11 07:53:56 +0900

    [script] change normal/bold font weight on MPDPlayer script
    Previously, bold text is not so clearly highlighted compared to normal text.
    Now it's fixed, bold highlighting is enough highlighted.

--- scripts/mpdplayer.sh
+++ scripts/mpdplayer.sh
@@ -2,6 +2,8 @@
 
 options=(
   --class kitty-music
+  --override font_family="Iosevka Term"
+  --override bold_font="Iosevka Term Heavy"
   --override background_opacity=0.7
   --override allow_remote_control=yes
   --override enabled_layouts='*'
