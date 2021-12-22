0ee45159622b8e90808e42154d7354748408b73b
miyakogi <miyako.dev@gmail.com>
2021-12-22 22:19:10 +0900

    [script] force chrome to use Vulkan-ANGLE instead of EGL on Xwayland
    - On Xorg session, Chrome still uses EGL (by default it uses ANGLE)
    - For both on Xwayland and Xorg, use Vulkan and Ozone

--- scripts/chrome.sh
+++ scripts/chrome.sh
@@ -8,14 +8,16 @@ else
 fi
 
 flags=(
+  # Enable HW video acceleration
   --enable-accelerated-video
   --enable-accelerated-video-decoder
+  # Force GPU Acceleration
+  # see https://wiki.archlinux.org/title/Chromium#Force_GPU_acceleration
   --ignore-gpu-blocklist
   --enable-gpu-rasterization
   --enable-zero-copy
-  --use-gl=egl
 )
-features="VaapiVideoDecoder"
+features="VaapiVideoDecoder,CanvasOopRasterization,EnableDrDc,RawDraw"
 options=("$@")
 
 if [[ $1 == "wayland" ]]; then
@@ -27,10 +29,25 @@ if [[ $1 == "wayland" ]]; then
   )
   options=("${options[2,-1]}")  # remove first option ($1)
 else
+  features="$features,Vulkan"
   flags+=(
     --enable-features="$features"
-    --disable-features="UseOzonePlatform"
   )
+  if [[ $session == "wayland" ]]; then
+    # Xwayland
+    flags+=(
+      # Force to use ANGLE to fix Xwayland issues, especially on NVIDIA GPU (but also useful on AMD GPU)
+      # see https://wiki.archlinux.org/title/chromium#Running_on_XWayland
+      --use-angle=vulkan
+      --use-cmd-decoder=passthrough
+    )
+  else
+    # Xorg
+    flags+=(
+      # GL is broken on Xwayland
+      --use-gl=egl
+    )
+  fi
 fi
 
 exec google-chrome-stable "${flags[@]}" "${options[@]}"
