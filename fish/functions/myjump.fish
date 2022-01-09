2396fe256f4b909e8dc03b2f55fdcb639aabfde7
miyakogi <miyako.dev@gmail.com>
2022-01-10 08:20:14 +0900

    [fish] surround variable on test

--- fish/functions/myjump.fish
+++ fish/functions/myjump.fish
@@ -1,7 +1,7 @@
 function myjump
-  if not test -z $_search_cmd
+  if test -n "$_search_cmd"
     set -l destination (cat $HIST_DIRS_FILE | eval "$_search_cmd")
-    test -z $destination; and return
+    test -z "$destination"; and return
     commandline -b "cd \"$destination\""
     commandline -f execute
   else
