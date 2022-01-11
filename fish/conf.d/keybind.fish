774ed1c151a682ecdca1874099cfb83103d64d54
miyakogi <miyako.dev@gmail.com>
2022-01-12 06:37:39 +0900

    [fish] Delete single-word by Ctrl+w

--- fish/conf.d/keybind.fish
+++ fish/conf.d/keybind.fish
@@ -1,6 +1,7 @@
 #!/usr/bin/env fish
 
 function fish_user_key_bindings
+  bind \cw backward-kill-word
   bind \cy 'commandline "cd ../" ; commandline -f execute'
   bind \cj myjump
   bind \cr search-history
