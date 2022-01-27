8860df7a634f38bcf2f6071afa0155f131dd85d3
miyakogi <miyako.dev@gmail.com>
2022-01-27 11:32:37 +0900

    [script] mpdplayer use sarasa instead of iosevka

--- scripts/mpdplayer.sh
+++ scripts/mpdplayer.sh
@@ -2,8 +2,9 @@
 
 options=(
   --class kitty-music
-  --override font_family="Iosevka Term"
-  --override bold_font="Iosevka Term Heavy"
+  --override font_family="Sarasa Term J"
+  --override bold_font="Sarasa Term J Bold"
+  --override font_size="13.5"
   --override background_opacity=0.7
   --override allow_remote_control=yes
   --override enabled_layouts='*'
