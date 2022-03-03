c4963d3275a9349dad4037ca6d1be46277c40076
miyakogi <miyako.dev@gmail.com>
2022-03-03 09:44:13 +0900

    [sway] use foot for dropdown terminal instead of alacritty

--- i3/scratchterm.py
+++ i3/scratchterm.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import subprocess
 import time
 from typing import Optional
@@ -7,8 +8,37 @@ from typing import Optional
 from i3ipc import Connection, Con
 
 i3 = Connection()
-TERM = 'alacritty'
 TERM_CLASS = 'scratchterm'
+
+# --- Define Command for Dropdown Terminal
+# Example (kitty without tmux):
+# TERM_CMD = [
+#     'kitty',
+#     '--class', TERM_CLASS,
+#     '--override', 'background_opacity=0.85',
+#     '--override', 'window_padding_width=2',
+# ]
+if os.getenv('WAYLAND_SOCKET') or os.getenv('XDG_SESSION_TYPE') == 'wayland':
+    # on wayland, use foot with tmux
+    TERM_CMD = [
+        'foot',
+        '--app-id', TERM_CLASS,
+        '--term', 'xterm-256color',  # need true color support
+        '--override=colors.alpha=0.7',
+        '--override=pad=2x2',
+        'scratchterm-tmux',
+    ]
+else:
+    # on Xorg, use alacritty with tmux
+    TERM_CMD = [
+        'alacritty',
+        '--class', TERM_CLASS,
+        '--option', 'env.TERM=xterm-256color',  # need true color support
+        '--option', 'window.opacity=0.7',
+        '--option', 'window.padding.x=2',
+        '--option', 'window.padding.y=2',
+        '--command', 'scratchterm-tmux',
+    ]
 
 
 def get_window() -> Optional[Con]:
@@ -23,21 +53,17 @@ def main() -> None:
     window = get_window()
 
     if window is None:
-        subprocess.Popen([
-            TERM,
-            '--class', TERM_CLASS,
-            '--option', 'env.TERM=xterm-256color',  # need true color support
-            '--option', 'window.opacity=0.7',
-            '--option', 'window.padding.x=2',
-            '--option', 'window.padding.y=2',
-            '--command', 'scratchterm-tmux',
-        ])
+        # start dropdown terminal
+        subprocess.Popen(TERM_CMD)
+
+        # wait until terminal window appears (timeout: 1sec)
         for _ in range(100):
             time.sleep(0.01)
             window = get_window()
             if window is not None:
                 break
 
+        # maybe failed to start dropdown terminal
         if window is None:
             return
 
