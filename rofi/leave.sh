49303c179d45573eac6aa4e713126ca6b0f387dc
miyakogi <miyako.dev@gmail.com>
2022-07-26 09:00:10 +0900

    [rofi] add `suspend` to leave menu

--- rofi/leave.sh
+++ rofi/leave.sh
@@ -4,11 +4,17 @@
 WM=$(wmctrl -m | grep "Name" | sed 's/^Name: \(.\+\)$/\1/' | tr '[:upper:]' '[:lower:]')
 
 # show menu by rofi (first space is trimmed by sh/bash, so use ASCII code (0x20))
-menu="\x20Lock\0icon\x1fsystem-lock-screen\n Exit\0icon\x1fsystem-logout\n Restart\0icon\x1fsystem-reboot\n Shutdown\0icon\x1fsystem-shutdown"
+menu="\x20Lock\0icon\x1fsystem-lock-screen\n Suspend\0icon\x1fsystem-lock-screen\n Exit\0icon\x1fsystem-log-out\n Restart\0icon\x1fsystem-reboot\n Shutdown\0icon\x1fsystem-shutdown"
 RET=$(echo -en $menu | rofi -dmenu -i -p "Select" -theme leave-theme | tr -d '[:space:]')
 
 _lock() {
   lock-screen
+}
+
+_suspend() {
+  lock-screen
+  sleep 3
+  systemctl suspend
 }
 
 _exit() {
@@ -36,6 +42,8 @@ _shutdown() {
 case $RET in
   Lock)
     _lock;;
+  Suspend)
+    _suspend;;
   Exit)
     _exit;;
   Restart)
