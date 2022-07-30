22f3c32beaecd40a012fe34c2bf715a08395e4e8
miyakogi <miyako.dev@gmail.com>
2022-07-31 06:16:00 +0900

    [script] improve code readability

--- scripts/launch-foot.sh
+++ scripts/launch-foot.sh
@@ -2,7 +2,7 @@
 
 # Luanch foot terminal with different font size according to output name
 
-if [[ $(swaymsg -t get_outputs | jq '.[]  | select(.focused) | .name') == \"DP-1\" ]]; then
+if [[ $(swaymsg -t get_outputs | jq '.[]  | select(.focused) | .name') == '"DP-1"' ]]; then
   foot --override=font="Sarasa Term J:weight=Light:size=22.5" --override=pad=12x12
 else
   foot
