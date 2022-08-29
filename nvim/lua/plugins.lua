9089879208b97a0325096b3fad0add81f811c88d
miyakogi <miyako.dev@gmail.com>
2022-08-29 17:44:46 +0900

    [nvim] plugin: add input method (fcitx/fcitx5) control plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -4,6 +4,9 @@ return require('packer').startup(function(use)
   -- manage packer.nvim itself
   use 'wbthomason/packer.nvim'
 
+  -- input method (fcitx/fcitx5 control)
+  use 'h-hg/fcitx.nvim'
+
 end)
 
 -- vim: set sw=2 et
