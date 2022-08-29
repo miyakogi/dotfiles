eb4d3d502d8668a17b539778caf5083acd1fdc88
miyakogi <miyako.dev@gmail.com>
2022-08-29 22:40:00 +0900

    [nvim] plugin: add gitsigns plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -66,6 +66,40 @@ return require('packer').startup(function(use)
     end,
   }
 
+  -- git integration
+  use {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup({
+        on_attach = function(bufnr)
+          -- navigation key mappings
+          local gs = package.loaded.gitsigns
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          map('n', ']c', function()
+            if vim.wo.diff then
+              return ']c'
+            end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+          end, { expr = true })
+
+          map('n', '[c', function()
+            if vim.wo.diff then
+              return '[c'
+            end
+            vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+          end, { expr = true })
+        end,
+      })
+    end,
+  }
+
   -- spellchecker
   use {
     'lewis6991/spellsitter.nvim',
