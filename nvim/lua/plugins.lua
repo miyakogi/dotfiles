a32eeaa928758e3973c468156cb8d005257a88ca
miyakogi <miyako.dev@gmail.com>
2022-08-30 13:40:05 +0900

    [nvim] plugin: add icon on lsp-completion text

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -264,11 +264,34 @@ return require('packer').startup(function(use)
     end,
   }
 
+  -- show icon on lsp types
+  use {
+    'onsails/lspkind.nvim',
+    opt = true,
+    event = 'InsertEnter',
+    requires = { 'hrsh7th/nvim-cmp' },
+    config = function()
+      local lspkind = require('lspkind')
+      require('cmp').setup {
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = 'symbol_text', -- show symbol and text
+            maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+
+            -- The function below will be called before any actual modifications from lspkind
+            -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+            before = function (entry, vim_item)
+              return vim_item
+            end
+          })
+        }
+      }
+    end,
+  }
+
   -- completion
   use {
     'hrsh7th/nvim-cmp',
-    -- opt = true,
-    -- event = 'InsertEnter',
     requires = {
         { 'neovim/nvim-lspconfig' },
         { 'hrsh7th/cmp-nvim-lsp' },
