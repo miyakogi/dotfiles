5acaf636e55a78722a5a2bb6761a6cdb8d5c2c44
miyakogi <miyako.dev@gmail.com>
2022-09-17 14:00:47 +0900

    [nvim] add necessary config for zk-nvim on tree-sitter

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -60,7 +60,10 @@ return require('packer').startup(function(use)
           -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
           -- Using this option may slow down your editor, and you may see some duplicate highlights.
           -- Instead of true it can also be a list of languages
-          additional_vim_regex_highlighting = false,
+          additional_vim_regex_highlighting = {
+            -- need for zk-nvim
+            'markdown',
+          },
         },
       })
     end,
