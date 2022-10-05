c4052838128e86c43b87f44f00a7a79c48e889cd
miyakogi <miyako.dev@gmail.com>
2022-10-05 17:04:24 +0900

    [alacritty] fix font style

--- scripts/launch-alacritty.sh
+++ scripts/launch-alacritty.sh
@@ -3,7 +3,7 @@
 # Luanch alacritty terminal with different font size according to output name
 
 if [[ $(swaymsg -t get_outputs | jq '.[]  | select(.focused) | .name') == '"DP-1"' ]]; then
-  alacritty --option=font.size=22.5 --option=window.padding.x=12 --option=window.padding.y=12
+  alacritty --option=font.normal.style=Regular --option=font.size=22.5 --option=window.padding.x=12 --option=window.padding.y=12
 else
   alacritty
 fi
