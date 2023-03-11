cf5b3569465b1352e7580f84901ed5d781d9db4e
miyakogi <miyako.dev@gmail.com>
2023-03-11 13:22:55 +0900

    [bar] fix and improve firefox volume check script

--- scripts/ff-volume-watch.sh
+++ scripts/ff-volume-watch.sh
@@ -4,22 +4,20 @@ tmp_file="/tmp/ff-volume-fix-paused"
 
 # check if disabled
 if [[ -e "$tmp_file" ]]; then
-  echo -n "  罹"  # nf-mdi-sync_alert
-  exit 0
-fi
-
-# check if Firefox is playing
-if [ "$(playerctl -p firefox status)" = "Playing" ]; then
-  modified_ff_sinks=$(pulsemixer --list-sinks | grep 'Name: Firefox' | grep -v '100%')
-  if [[ -n "$modified_ff_sinks" ]]; then
-    echo "$modified_ff_sinks" | while read -r line; do
-      ff_sink_id=$(echo "$line" | sed -r 's/^.*ID: ([a-zA-Z0-9\-]+).*$/\1/')
-      pulsemixer --id "$ff_sink_id" --set-volume 100
-    done
+  echo '{"text": "  罹"}'  # nf-mdi-sync_alert
+else
+  # check if Firefox is playing
+  if playerctl -l 2>&1 | grep firefox && [[ "$(playerctl -p firefox status)" == "Playing" ]]; then
+    modified_ff_sinks=$(pulsemixer --list-sinks | grep 'Name: Firefox' | grep -v '100%')
+    if [[ -n "$modified_ff_sinks" ]]; then
+      echo "$modified_ff_sinks" | while read -r line; do
+        ff_sink_id=$(echo "$line" | sed -r 's/^.*ID: ([a-zA-Z0-9\-]+).*$/\1/')
+        pulsemixer --id "$ff_sink_id" --set-volume 100
+      done
+    fi
   fi
+  echo '{"text": "  "}'
 fi
-
-echo -n "  "
 
 sleep 0.1s
 
