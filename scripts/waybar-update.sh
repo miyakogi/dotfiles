c34e5668a0f6f814b1b7b614a4f63e51489310fa
miyakogi <miyako.dev@gmail.com>
2023-03-17 09:24:08 +0900

    [script] fix pacman update counts

--- scripts/waybar-update.sh
+++ scripts/waybar-update.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/bash
 
-num=$(($(pacman -Qu | wc -l)+$(paru -Qua | grep -c -v '\[ignored\]')))
+# Pacman DB path
+dbpath="/tmp/checkup-db-waybar"
+local_dbpath="$dbpath/local"
+
+# Create DB link if not exist
+if [ ! -e "$local_dbpath" ]; then
+  ln -s /var/lib/pacman "$local_dbpath" &>/dev/null
+fi
+
+# Update database
+fakeroot -- pacman -Sy --dbpath /tmp/checkup-db-waybar --logfile /dev/null &>/dev/null
+
+# Count updates
+# pacman
+num_pacman=$(fakeroot -- pacman -Qu --dbpath /tmp/checkup-db-waybar | wc -l)
+# AUR
+num_aur=$(paru -Qua | grep -c -v '\[ignored\]')
+# Both
+num=$(("$num_pacman"+"$num_aur"))
 
 if [ "$num" -gt 0 ]; then
   echo "{\"text\": \" $num\", \"class\": \"info\"}"
