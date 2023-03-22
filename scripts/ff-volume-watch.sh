8b4bec2be8730a46ccd8427cc9cb85d830e8de91
miyakogi <miyako.dev@gmail.com>
2023-03-23 08:17:54 +0900

    [script] turn off message in specific case

--- scripts/ff-volume-watch.sh
+++ scripts/ff-volume-watch.sh
@@ -7,7 +7,7 @@ if [[ -e "$tmp_file" ]]; then
   echo '{"text": "  罹"}'  # nf-mdi-sync_alert
 else
   # check if Firefox is playing
-  if playerctl -l 2>&1 | grep firefox && [[ "$(playerctl -p firefox status)" == "Playing" ]]; then
+  if playerctl -l 2>&1 | grep -q firefox && [[ "$(playerctl -p firefox status)" == "Playing" ]]; then
     modified_ff_sinks=$(pulsemixer --list-sinks | grep 'Name: Firefox' | grep -v '100%')
     if [[ -n "$modified_ff_sinks" ]]; then
       echo "$modified_ff_sinks" | while read -r line; do
