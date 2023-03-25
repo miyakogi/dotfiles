6e0695d86acb593abc4407013632229680cbc1bb
miyakogi <miyako.dev@gmail.com>
2023-03-25 15:40:42 +0900

    [script] improve kitty's startup time
    Fixing OpenGL driver by env var improves startup time as same as
    second startup with --single-instance option

--- scripts/launch-kitty.sh
+++ scripts/launch-kitty.sh
@@ -2,9 +2,9 @@
 
 options=()
 if [ "$(is-4k)" = true ]; then
-  options+=(--single-instance --instance-group 'HiDPI' --override=font_size=21 --override=window_padding_width=12)
-else
-  options+=(--single-instance --instance-group 'FHD')
+  options+=(--override=font_size=21 --override=window_padding_width=12 --override=window_border_width=4)
 fi
 
-kitty "${options[@]}"
+# Fixing driver improve startup time
+# see: https://sw.kovidgoyal.net/kitty/faq/#why-does-kitty-sometimes-start-slowly-on-my-linux-system
+env MESA_LOADER_DRIVER_OVERRIDE=radeonsi __EGL_VENDOR_LIBRARY_FILENAMES=/usr/share/glvnd/egl_vendor.d/50_mesa.json kitty "${options[@]}"
