f9fd2132db01bd75d5ca80c031c3bf93ebb4667d
miyakogi <miyako.dev@gmail.com>
2023-04-01 12:14:23 +0900

    [alacritty] use IBM Plex Mono font

--- scripts/launch-alacritty.sh
+++ scripts/launch-alacritty.sh
@@ -4,7 +4,7 @@
 
 options=()
 if [ "$(is-4k)" = true ]; then
-  options+=(--option=font.normal.style=Regular --option=font.size=21 --option=window.padding.x=12 --option=window.padding.y=12)
+  options+=(--option=font.normal.style=Text --option=font.size=21.0 --option=window.padding.x=12 --option=window.padding.y=12)
 fi
 
 alacritty "${options[@]}"
