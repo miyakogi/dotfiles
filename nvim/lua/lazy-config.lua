b20cf0b68a04cee55667dd1a5ef4a638da4acad6
miyakogi <miyako.dev@gmail.com>
2023-05-05 11:44:27 +0900

    [nvim] install peek.nvim only when deno is available

--- nvim/lua/lazy-config.lua
+++ nvim/lua/lazy-config.lua
@@ -676,36 +676,6 @@ local plugins = {
     end,
   },
 
-  -- markdown preview
-  {
-    'toppair/peek.nvim',
-    build = 'deno task --quiet build:fast',
-    lazy = true,
-    cmd = 'PeekOpen',
-    -- filetype = 'markdown',
-    config = function ()
-      require('peek').setup({
-        auto_load = true,
-        close_on_bdelete = true,
-        syntax = true,
-        theme = 'dark',
-        update_on_change = true,
-
-        -- relevant if update_on_change is true
-        throttle_at = 200000,     -- start throttling when file exceeds this
-                                  -- amount of bytes in size
-        throttle_time = 'auto',   -- minimum amount of time in milliseconds
-                                  -- that has to pass before starting new render
-      })
-
-      -- add command
-      vim.api.nvim_create_user_command('PeekOpen', function()
-        require('peek').open()
-      end, {})
-      vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
-    end
-  },
-
   -- ### ColorScheme ###
   {
     'EdenEast/nightfox.nvim',
@@ -816,5 +786,37 @@ local plugins = {
     event = 'InsertEnter',
   },
 }
+
+if vim.fn.executable('deno') then
+  -- markdown preview
+  plugins[#plugins+1] = {
+    'toppair/peek.nvim',
+    build = 'deno task --quiet build:fast',
+    lazy = true,
+    cmd = 'PeekOpen',
+    -- filetype = 'markdown',
+    config = function ()
+      require('peek').setup({
+        auto_load = true,
+        close_on_bdelete = true,
+        syntax = true,
+        theme = 'dark',
+        update_on_change = true,
+
+        -- relevant if update_on_change is true
+        throttle_at = 200000,     -- start throttling when file exceeds this
+                                  -- amount of bytes in size
+        throttle_time = 'auto',   -- minimum amount of time in milliseconds
+                                  -- that has to pass before starting new render
+      })
+
+      -- add command
+      vim.api.nvim_create_user_command('PeekOpen', function()
+        require('peek').open()
+      end, {})
+      vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
+    end,
+  }
+end
 
 require('lazy').setup(plugins)
