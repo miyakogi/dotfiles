ee7080b11aceda9ae5290ff8c7dfbdf36c3d6bd3
miyakogi <miyako.dev@gmail.com>
2023-06-06 01:27:09 +0900

    [fish] fix grouping of conditional expression

--- fish/functions/ls.fish
+++ fish/functions/ls.fish
@@ -1,7 +1,7 @@
 function ls
-  if type -q exa; and test -n "$DISPLAY"; or test -z "$XDG_VTNR"
+  if type -q exa; and begin; test -n "$DISPLAY"; or test -z "$XDG_VTNR"; end
     command exa --icons --group-directories-first --sort Name $argv
-  else if type -q lsd; and test -n "$DISPLAY"; or test -z "$XDG_VTNR"
+  else if type -q lsd; and begin; test -n "$DISPLAY"; or test -z "$XDG_VTNR"; end
     command lsd
   else
     command ls -v --color --group-directories-first
