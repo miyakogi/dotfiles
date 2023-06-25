bc0b3fbfcf31ebbe82d84755f11989923349b028
miyakogi <miyako.dev@gmail.com>
2023-06-25 20:35:01 +0900

    [alacritty] use 4K config as default

--- scripts/launch-alacritty.sh
+++ scripts/launch-alacritty.sh
@@ -3,8 +3,8 @@
 # Luanch alacritty terminal with different font size according to output name
 
 options=()
-if [ "$(is-4k)" = true ]; then
-  options+=(--option=font.normal.style=Regular --option=font.size=19.5 --option=window.padding.x=12 --option=window.padding.y=12)
+if [ ! "$(is-4k)" = true ]; then
+  options+=(--option=font.normal.style=Text --option=font.size=12.0 --option=window.padding.x=8 --option=window.padding.y=8)
 fi
 
 alacritty "${options[@]}" "$@"
