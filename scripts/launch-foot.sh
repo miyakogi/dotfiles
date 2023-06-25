a1f402f2dfb5701439b904012fa72663e57c6855
miyakogi <miyako.dev@gmail.com>
2023-06-25 20:27:59 +0900

    [foot] use 4K config as default

--- scripts/launch-foot.sh
+++ scripts/launch-foot.sh
@@ -3,8 +3,8 @@
 # Luanch foot terminal with different font size according to output name
 
 options=()
-if [ "$(is-4k)" = true ]; then
-  options+=(--override=font="IBM Plex Mono:size=19.5" --override=font-bold="IBM Plex Mono SmBld:size=19.5" --override=pad=12x12)
+if [ ! "$(is-4k)" = true ]; then
+  options+=(--override=font="IBM Plex Mono:size=12.0" --override=font-bold="IBM Plex Mono SmBld:size=12.0" --override=pad=8x8)
 fi
 
 foot "${options[@]}" "$@"
