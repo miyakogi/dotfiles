f2d02d0898b80a72114764b31299d6334abde017
miyakogi <miyako.dev@gmail.com>
2023-06-25 20:37:24 +0900

    [kitty] use 4K config as default

--- scripts/launch-kitty.sh
+++ scripts/launch-kitty.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 options=()
-if [ "$(is-4k)" = true ]; then
-  options+=(--override=font_size=19.5 --override=window_padding_width=12 --override=window_border_width=4)
+if [ ! "$(is-4k)" = true ]; then
+  options+=(--override=font_size=12.0 --override=window_padding_width=8 --override=window_border_width=2px)
 fi
 
 # Fixing driver improve startup time
