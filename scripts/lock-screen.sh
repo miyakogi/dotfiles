0712307d1c2c9ca477902412dcf323d900c4bc45
miyakogi <miyako.dev@gmail.com>
2023-07-28 11:21:59 +0900

    [script] change font for lock screen

--- scripts/lock-screen.sh
+++ scripts/lock-screen.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if ! type swaylock &>/dev/null; then
-  echo "\`waylock\` should be installed to lock screen"
+  echo "\`swaylock-effects\` should be installed to lock screen"
   exit 1
 fi
 
@@ -9,6 +9,7 @@ cmd=(
   swaylock
   --daemonize
   --screenshots
+  --font 'Liberation Sans'
   --clock
   --datestr '%b. %-d %Y'
   --indicator
