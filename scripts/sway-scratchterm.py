5ae3851facc907d12494731d7c312ff219a2fda5
miyakogi <miyako.dev@gmail.com>
2023-07-28 11:06:07 +0900

    [script] change font for scratch terminal on sway

--- scripts/sway-scratchterm.py
+++ scripts/sway-scratchterm.py
@@ -17,10 +17,10 @@ TERM_CMD = [
     "--override=colors.alpha=0.85",
 ]
 
-override_font = "--override=font=IBM Plex Mono:size="
+override_font = "--override=font=JetBrainsMono Nerd Font"
 
-TERM_CMD1 = TERM_CMD + ["--app-id", TERM_CLASS1, override_font + "21"]
-TERM_CMD2 = TERM_CMD + ["--app-id", TERM_CLASS2, override_font + "12"]
+TERM_CMD1 = TERM_CMD + ["--app-id", TERM_CLASS1, override_font + ":size=21"]
+TERM_CMD2 = TERM_CMD + ["--app-id", TERM_CLASS2, override_font + ":size=12"]
 
 
 def get_window() -> Optional[Con]:
