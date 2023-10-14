b4c09de1eb4aa165a59ed3773be0d2ba1d1a26b8
miyakogi <miyako.dev@gmail.com>
2023-10-15 03:28:07 +0900

    [fish] use zoxide to jump directory

--- fish/functions/myjump.fish
+++ fish/functions/myjump.fish
@@ -1,11 +1,16 @@
 function myjump
-  if test -n "$_search_cmd"
-    set -l destination (cat $HIST_DIRS_FILE | eval "$_search_cmd")
-    test -z "$destination"; and return
-    commandline -b "cd \"$destination\""
-    commandline -f execute
-  else
-    echo -e "Install `fzf` to use jump dir shortcut\n\n"
+  if type -q zi
+    zi ""
     commandline -f repaint
+  else
+    if test -n "$_search_cmd"
+      set -l destination (cat $HIST_DIRS_FILE | eval "$_search_cmd")
+      test -z "$destination"; and return
+      commandline -b "cd \"$destination\""
+      commandline -f execute
+    else
+      echo -e "Install `zoxide` or `fzf` to use jump dir shortcut\n\n"
+      commandline -f repaint
+    end
   end
 end
