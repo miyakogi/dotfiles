1f192613613c902acf8bd0db85a9edcd843ab20e
miyakogi <miyako.dev@gmail.com>
2023-10-15 04:39:33 +0900

    [script] always fix firefox volume

--- scripts/ff-volume-watch.sh
+++ scripts/ff-volume-watch.sh
@@ -7,7 +7,7 @@ if [ -e "$tmp_file" ]; then
   echo -e '{"text": " \u2005 \u2005"}'  # nf-cod-sync_ignored + 1/4 rem unicode space (U+2005)
 else
   # check if Firefox is playing
-  if playerctl -l 2>&1 | grep -q firefox && [ "$(playerctl -p firefox status)" = "Playing" ]; then
+  if playerctl -l 2>&1 | grep -q firefox; then
     modified_ff_sinks=$(pulsemixer --list-sinks | grep -e 'Name: Firefox' -e 'Name: Cachy Browser' | grep -v '100%')
     if [ -n "$modified_ff_sinks" ]; then
       echo "$modified_ff_sinks" | while read -r line; do
