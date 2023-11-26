40a135dc82c642e74ed32c0214fb02a23ce6be1c
miyaco <miyako.dev@gmail.com>
2023-11-27 06:02:52 +0900

    [kitty] use smaller font on sub monitor

--- scripts/launch-kitty.sh
+++ scripts/launch-kitty.sh
@@ -3,8 +3,8 @@
 options=()
 if ! is-4k; then
   options+=(
-    --override=font_size=12.0
-    --override=window_padding_width=8
+    --override=font_size=10.5
+    --override=window_padding_width=4
     --override=window_border_width=2px
   )
 fi
