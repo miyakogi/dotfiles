8d80ea2bb5b4a4e4a6d9590b5be76cede18eeb3e
miyaco <miyako.dev@gmail.com>
2023-11-27 06:02:24 +0900

    [foot] use smaller font on sub monitor

--- scripts/launch-foot.sh
+++ scripts/launch-foot.sh
@@ -5,8 +5,8 @@
 options=()
 if ! is-4k; then
   options+=(
-    --override=font="IntoneMono Nerd Font:size=12.0"
-    --override=font-bold="IntoneMono Nerd Font:weight=800:size=12.0"
+    --override=font="IntoneMono Nerd Font:size=10.5"
+    --override=font-bold="IntoneMono Nerd Font:weight=800:size=10.5"
     --override=pad="6x0 center"
   )
 fi
