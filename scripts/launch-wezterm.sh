9bc3f8966ca912444e0d950f8f9085f92e37089f
miyaco <miyako.dev@gmail.com>
2023-12-30 18:24:33 +0900

    [script] support wezterm class

--- scripts/launch-wezterm.sh
+++ scripts/launch-wezterm.sh
@@ -6,6 +6,14 @@ if ! is-4k; then
     --config-file="$HOME/.config/wezterm/wezterm_fhd.lua"
   )
 fi
-options+=(start --class wezterm)
+options+=(start)
+
+if [ "$1" = "--class" ]; then
+  options+=("$1" "$2")
+  shift
+  shift
+else
+  options+=(--class wezterm)
+fi
 
 wezterm "${options[@]}" "$@"
