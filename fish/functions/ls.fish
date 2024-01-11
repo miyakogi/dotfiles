87654b6f930baced1e1d2921d1ddeea593b80aa8
miyaco <miyako.dev@gmail.com>
2024-01-11 13:36:31 +0900

    [fish] add group column on ls long view

--- fish/functions/ls.fish
+++ fish/functions/ls.fish
@@ -2,7 +2,7 @@ function ls
   # if type -q lsd; and begin; test -n "$DISPLAY"; or test -z "$XDG_VTNR"; end
   if test -n "$DISPLAY"; or test -z "$XDG_VTNR"
     if type -q eza
-      command eza --icons=auto --group-directories-first --sort Filename --time-style "+%Y-%m-%d %H:%M" $argv
+      command eza --icons=auto --group-directories-first --sort Filename --group --time-style "+%Y-%m-%d %H:%M" $argv
     else if type -q lsd
       command lsd $argv
     else
