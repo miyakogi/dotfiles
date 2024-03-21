7c0a3f86445109df172a5f3d4f3b52845fd0ddc4
miyaco <miyako.dev@gmail.com>
2024-03-22 08:16:40 +0900

    [fish] add Ctrl+Backspace keybind

--- fish/conf.d/keybind.fish
+++ fish/conf.d/keybind.fish
@@ -2,6 +2,7 @@
 
 function fish_user_key_bindings
   bind \cw backward-kill-word
+  bind \b backward-kill-word  # Ctrl+Backspace
   bind \cy 'commandline "cd ../" ; commandline -f execute'
   bind \cj myjump
   bind \cf nextd-or-forward-word
