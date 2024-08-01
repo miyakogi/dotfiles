27fcccdf0a4df954f890fde46bd544735e70106e
miyaco <miyako.dev@gmail.com>
2024-08-01 23:53:42 +0900

    [nvim] add sniprun

--- nvim/lua/lazy-config.lua
+++ nvim/lua/lazy-config.lua
@@ -466,6 +466,22 @@ local plugins = {
     end,
   },
 
+  {
+    "michaelb/sniprun",
+    branch = "master",
+
+    build = "sh install.sh",
+    -- do 'sh install.sh 1' if you want to force compile locally
+    -- (instead of fetching a binary from the github release). Requires Rust >= 1.65
+
+    config = function()
+      require("sniprun").setup({
+      -- your options
+      })
+      vim.api.nvim_set_keymap('v', '<leader>r', '<Plug>SnipRun', {silent = true})
+    end,
+  },
+
   -- linter
   {
     'mfussenegger/nvim-lint',
