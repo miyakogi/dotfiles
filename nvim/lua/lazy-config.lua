ba08edc97ada0fa4ce10d94e22f80a63e93f863e
miyaco <miyako.dev@gmail.com>
2024-08-01 23:59:34 +0900

    [nvim] rose-pine colorscheme

--- nvim/lua/lazy-config.lua
+++ nvim/lua/lazy-config.lua
@@ -686,15 +686,22 @@ local plugins = {
 
   -- ### ColorScheme ###
   {
-    'https://codeberg.org/miyaco/iceberg-tokyo.nvim',
+    'rose-pine/neovim',
+    name = 'rose-pine',
     config = function()
-      vim.cmd.colorscheme('iceberg-tokyo')
-      vim.cmd([[
-        highlight Normal ctermbg=NONE guibg=NONE
-        highlight LineNr ctermbg=NONE guibg=NONE
-        highlight SignColumn ctermbg=NONE guibg=NONE
-        highlight NonText ctermbg=NONE guibg=NONE
-      ]])
+      require('rose-pine').setup({
+        variant = 'main',
+        dim_inactive_windows = false,
+        styles = {
+          italic = false,
+          transparency = true,
+        },
+        highlight_groups = {
+          Comment = { fg = 'subtle', italic = true, },
+          htmlItalic = { italic = true, },
+        },
+      })
+      vim.cmd.colorscheme('rose-pine')
     end,
   },
 
