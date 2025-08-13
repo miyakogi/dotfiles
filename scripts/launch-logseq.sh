15c4d76e5ae8e9f21cf64df31882b402d4e35e41
miyaco <miyako.dev@gmail.com>
2025-08-13 17:42:16 +0900

    [script] logseq: support logseq-desktop-electron-bin package

--- scripts/launch-logseq.sh
+++ scripts/launch-logseq.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-exec app2unit -- logseq "$(chromium-options wayland)"
+if type logseq &>/dev/null; then
+  cmd=logseq
+elif type logseq-desktop-electron &>/dev/null; then
+  cmd=logseq-desktop-electron
+else
+  echo "Logseq Not Found"
+  notify-send -u critical "Command Not Found Error" "Logseq Not Found"
+  exit 1
+fi
+exec app2unit -- "$cmd" "$(chromium-options wayland)"
