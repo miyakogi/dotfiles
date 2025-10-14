5a0f2f8ab7723671d18b9f72361846e7e49047da
miyaco <miyako.dev@gmail.com>
2025-10-14 14:55:35 +0900

    [wezterm] change theme to erebus

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -105,11 +105,11 @@ return {
 
   detect_password_input = true,  -- show password icon
 
-  window_background_opacity = 0.95,
+  window_background_opacity = 0.90,
 
   -- Tab bar
   use_fancy_tab_bar = false,
-  color_scheme = 'Kanagawa Dragon (Gogh)',
+  color_scheme = 'Erebus (terminal.sexy)',
 
   hide_tab_bar_if_only_one_tab = true,
 
