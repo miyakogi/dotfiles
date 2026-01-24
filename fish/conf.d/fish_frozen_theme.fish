42ad32e73eea89c5a4990af351553492b81ca799
miyaco <miyako.dev@gmail.com>
2026-01-24 13:14:03 +0900

    [fish] update for v4.3

---
+++ fish/conf.d/fish_frozen_theme.fish
@@ -0,0 +1,49 @@
+# This file was created by fish when upgrading to version 4.3, to migrate
+# theme variables from universal to global scope.
+# Don't edit this file, as it will be written by the web-config tool (`fish_config`).
+# To customize your theme, delete this file and see
+#     help interactive#syntax-highlighting
+# or
+#     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
+# for appropriate commands to add to ~/.config/fish/config.fish instead.
+# See also the release notes for fish 4.3.0 (run `help relnotes`).
+
+set --global fish_color_autosuggestion 999999
+set --global fish_color_cancel
+set --global fish_color_command brblue
+set --global fish_color_comment 999999
+set --global fish_color_cwd
+set --global fish_color_cwd_root red
+set --global fish_color_end brred
+set --global fish_color_error red
+set --global fish_color_escape brmagenta
+set --global fish_color_gray 6c7086
+set --global fish_color_history_current --bold
+set --global fish_color_host
+set --global fish_color_host_remote
+set --global fish_color_keyword brmagenta
+set --global fish_color_match --background=brblue
+set --global fish_color_normal white
+set --global fish_color_operator yellow
+set --global fish_color_option
+set --global fish_color_param bryellow
+set --global fish_color_quote brgreen
+set --global fish_color_redirection white
+set --global fish_color_search_match --background=normal
+set --global fish_color_selection --background=normal
+set --global fish_color_status f38ba8
+set --global fish_color_user
+set --global fish_color_valid_path --underline
+set --global fish_pager_color_background
+set --global fish_pager_color_completion white
+set --global fish_pager_color_description 999999
+set --global fish_pager_color_prefix cyan
+set --global fish_pager_color_progress 999999
+set --global fish_pager_color_secondary_background
+set --global fish_pager_color_secondary_completion
+set --global fish_pager_color_secondary_description
+set --global fish_pager_color_secondary_prefix
+set --global fish_pager_color_selected_background
+set --global fish_pager_color_selected_completion
+set --global fish_pager_color_selected_description
+set --global fish_pager_color_selected_prefix
